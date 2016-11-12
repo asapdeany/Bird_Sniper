ff39e69506378de7c04a3126e79c41c77960ef47
asapdeany <dspon15@stlawu.edu>
2016-11-11 20:18:49 -0500

    Added Icons for Android homescreen

--- app/src/main/java/com/example/deansponholz/fish_game/SensorData.java
+++ app/src/main/java/com/example/deansponholz/fish_game/SensorData.java
@@ -30,14 +30,12 @@ public class SensorData implements SensorEventListener {
     private Sensor mSensor;
     //Speed at which ball moves given by accelerometer data
     public double xPos, yPos;
-    //Custom view
-    MyDrawView myDrawing = null;
     //Size of screen
     int width;
     int height;
     //Starting position and keeps track of where ball is currently
-    public float x=360;
-    public float y=580;
+    public float x=0;
+    public float y=0;
     //Size of ball
     int size;
     //private SensorManager mSensorManager = null;
@@ -77,21 +75,11 @@ public class SensorData implements SensorEventListener {
     public static final float FILTER_COEFFICIENT = 0.98f;
     private Timer fuseTimer = new Timer();
 
-    public Handler mHandler;
-    private RadioGroup mRadioGroup;
-    private TextView mAzimuthView;
-    private TextView mPitchView;
-    private TextView mRollView;
-    private int radioSelection;
-    DecimalFormat d = new DecimalFormat("#.##");
-
-    public TextView yaw_test = null;
-
-    private int THRESHOLD;
-    private long prev_time = 0;
     //private final MainActivity ma;
     private final HUDFragment hudFragment;
 
+    //create sensor handler class
+    //use sensor handler class as paramter instead of hud fragment
     public SensorData(SensorManager sm, HUDFragment hudFragment){
 
         //this.ma = ma;
@@ -252,6 +240,7 @@ public class SensorData implements SensorEventListener {
         // get the gyroscope based orientation from the rotation matrix
         SensorManager.getOrientation(gyroMatrix, gyroOrientation);
     }
+
     private float[] getRotationMatrixFromOrientation(float[] o) {
         float[] xM = new float[9];
         float[] yM = new float[9];
@@ -362,105 +351,6 @@ public class SensorData implements SensorEventListener {
 
             // update sensor output in GUI
             //mHandler.post(updateOreintationDisplayTask);
-        }
-    }
-
-    public class MyDrawView extends View {
-        public MyDrawView(Context context) {
-            super(context);
-        }
-
-        public void onDraw(Canvas canvas) {
-            //Create a new paint color for the circle drawn later
-            width=canvas.getWidth();
-            height=canvas.getHeight();
-            Paint p = new Paint();
-            p.setColor(Color.MAGENTA);
-            size = 50;
-            //draw a circle at the point designated based on accelerometer data and previous points with a specified size and a color P
-            canvas.drawCircle(x + 340, y + 600, size, p);
-            //Move ball based on where ball is and accelerometer data
-            //Log.d("xxxx",Double.toString(xPos));
-            y= (float) (-xPos*15);
-            //Log.d("x",Float.toString(x));
-            x= (float) (yPos*15);
-            //Log.d("y",Float.toString(y));
-
-
-            //Log.i("xPos", String.valueOf(xPos));
-            //Log.i("yPos", String.valueOf(yPos));
-            //Check for collision and move ball back into bounds if collision is is found
-
-            if(topCollision(y))
-            {
-                y=size - 640;
-            }
-
-            if(leftCollision(x))
-            {
-                x=size -360;
-            }
-
-
-            if(rightCollision(x))
-            {
-                x=((width-310) - size);
-            }
-
-            if(bottomCollision(y))
-            {
-                y = height-size -550;
-            }
-
-            invalidate();
-        }
-    }
-    //Checks top of screen collision or if ball goes past
-    private boolean topCollision(float y)
-    {
-        if(y-(size - 640) <=0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    //Checks left of screen collision or if ball goes past
-    private boolean leftCollision(float x)
-    {
-        if((x+360) - size<=0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    //Checks right of screen collision or if ball goes past
-    private boolean rightCollision(float x)
-    {
-        if((x)+size>=width -310)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    //Checks bottom of screen collision or if ball goes past
-    private boolean bottomCollision(float y)
-    {
-        if(y+size>=height-550)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
         }
     }
 
